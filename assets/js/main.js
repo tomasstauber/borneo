c03c1e66620e2638750e6928acd11b86355d781d
Tomás Stauber <tomystauber@gmail.com>
2023-03-07 11:52:27 -0300

    trabajando en proyectofinal

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,43 @@ const iraPedidos = () =>{
 irPedidos.addEventListener("click", () =>{
     iraPedidos();
 })
+
+const loaderContainer = document.querySelector('.loading');
+let inicio = new Date;
+
+window.addEventListener('load', () => {
+    
+    let fin = new Date;
+    let segundos = (fin-inicio);
+
+    if(segundos < 2000){
+        setTimeout(() => {
+            loaderContainer.style.display = 'none';
+        }, 2000);
+    }else{
+        loaderContainer.style.display = 'none';
+    }
+});
+
+const formulario =document.getElementById("formulario");
+
+const reservar = (event) => {
+    event.preventDefault();
+    const datos = new FormData(event.target);
+    const datosCompletos = Object.fromEntries(datos.entries());
+    console.log(JSON.stringify(datosCompletos));
+}
+
+const procesaDatos = (event) => {
+    event.preventDefault();
+    const datos = new FormData(event.target);
+    const nombre = datos.get("nombre");
+    const email = datos.get("email");
+    const dni = datos.get("dni");
+    const fecha = datos.get("fecha");
+    const hora = datos.get("hora");
+    const cantidad = datos.get("cantidad");
+    console.log({nombre}, {email}, {dni}, {fecha}, {hora}, {cantidad})
+}
+
+formulario.addEventListener("submit", reservar);
