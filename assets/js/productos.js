dd95810b6e35e199bc2d8a241358338a3c44f64e
Tomás Stauber <tomystauber@gmail.com>
2023-03-10 15:12:45 -0300

    fetch

--- assets/js/productos.js
+++ assets/js/productos.js
@@ -1,37 +1,18 @@
-class Producto {
-    constructor(id, nombre, descripcion, precio, img) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.img = img;
-        this.cantidad = 1;
-    }
-};
-
-const papasFS = new Producto(1, "Papas Fritas", "Papas rústicas, fritas y condimentadas", 600, "../assets/img/papasFR.jpg");
-const papasCD = new Producto(2, "Papas con cheddar", "Papas rústicas fritas con cheddar y verdeo", 800, "../assets/img/papasFR.jpg");
-const hamburguesaBC = new Producto(3, "Hamburguesa BC", "Hamburguesa doble carne, con cheddar y bacon", 900, "../assets/img/papasFR.jpg");
-const hamburguesaPM = new Producto(4, "Hamburguesa Pampeana", "Hamburguesa doble carne, con queso tybo y salsa criolla", 1200, "../assets/img/papasFR.jpg");
-const tablaCH = new Producto(5, "Tabla Chica", "Tabla con variedad de quesos, fiambres y aceitunas", 1200, "../assets/img/papasFR.jpg");
-const tablaGR = new Producto(6, "Tabla Grande", "Tabla con variedad de quesos, fiambres. Incluye fondeu", 2500, "../assets/img/papasFR.jpg");
-const cervezaRB = new Producto(7, "Cerveza Rubia", "Tirada Artesanal", 600, "../assets/img/papasFR.jpg");
-const cervezaIPA = new Producto(8, "Cerveza IPA", "Tirada Artesanal", 600, "../assets/img/papasFR.jpg");
-const fernetBR = new Producto(9, "Fernet Branca", "Vaso de Fernet Branca 900cc.", 1000, "../assets/img/papasFR.jpg");
-const agua = new Producto(10, "Agua Mineral", "Agua mineral 500ml", 300, "../assets/img/papasFR.jpg");
-
-const menu = [papasFS, papasCD, hamburguesaBC, hamburguesaPM, tablaCH, tablaGR, cervezaRB, cervezaIPA, fernetBR, agua];
-
 let carrito = [];
 
 if (localStorage.getItem("carrito")) {
     carrito = JSON.parse(localStorage.getItem("carrito"));
 }
 
-const wrapper = document.getElementById("wrapper");
+const listado = document.getElementById("listado");
 
-const menuDinamico = () => {
-    menu.forEach(producto => {
+const listadoProductos = "../assets/json/productos.json";
+
+fetch(listadoProductos)
+.then(respuesta => respuesta.json())
+.then(datos =>  {
+    console.log(datos)
+    datos.forEach(producto => {
         const tarjeta = document.createElement("div");
         tarjeta.classList.add("tarjeta");
         tarjeta.innerHTML = `
@@ -46,36 +27,33 @@ const menuDinamico = () => {
         wrapper.appendChild(tarjeta);
 
         const agregar = document.getElementById(`agregar${producto.id}`);
-        agregar.addEventListener("click", () => {
-            agregarProducto(producto.id);
-            Toastify({
-                text: "Producto agregado al carrito!",
-                duration: 1000,
-                gravity: "top",
-                position: "right",
-                stopOnFocus: true,
-                style: {
-                    background: "#65B262",
-                }
-            }).showToast();
-        })
+            agregar.addEventListener("click", ()=> {
+                agregarProducto(producto.id);
+                Toastify({
+                    text: "Producto agregado al carrito!",
+                    duration: 1000,
+                    gravity: "top",
+                    position: "right",
+                    stopOnFocus: true,
+                    style: {
+                        background: "#65B262",
+                    }
+                }).showToast();
+            })
     })
-};
-
-menuDinamico();
+})
 
 const agregarProducto = (id) => {
     const productoAgregado = carrito.find(producto => producto.id === id);
     if (productoAgregado) {
         productoAgregado.cantidad++;
     } else {
-        const producto = menu.find(producto => producto.id === id);
+        const producto = listadoProductos.search(producto => producto.id === id);
         carrito.push(producto)
     }
     console.log(carrito);
     localStorage.setItem("carrito", JSON.stringify(carrito));
 };
-
 
 const wrapperCarrito = document.getElementById("wrapperCarrito");
 const verCarrito = document.getElementById("btnCarrito");
